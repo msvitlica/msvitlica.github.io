893ee8e0b510f6d50c0ca16502b4e44ca778ce57
Milan Svitlica <svitlicamilan@gmail.com>
2016-05-22 22:35:49 +0200

    some layout changes

--- Game.js
+++ Game.js
@@ -1,14 +1,22 @@
-var Game = function(){
+var GameSettings = function(){
+}
+
+GameSettings.maxNumber = 100;
+GameSettings.initialNumberOfLives = 3;
+
+
+var Game = function()
+{
 	console.log("instance of game created");
 	this.currentNumber = 0;
 	this.score = 0;
 	this.probability = 0;
-	this.probabilityScoore = 0;
-	this.lifes = 3;
+	this.probabilityScore = 0;
+	this.lives = GameSettings.initialNumberOfLives;
 
 	this.compare = function compare(option, nextNumber)
 	{
-		// 0 less, 1 more
+		// 0 less or equal, 1 more
 		if(option === 0)
 		{
 				return nextNumber <= this.currentNumber;
@@ -23,8 +31,8 @@ var Game = function(){
 		}
 	}
 
-	this.generateNumber = function(maxNum){
-		  return Math.floor((Math.random() * maxNum)+1);
+	this.generateNumber = function(){
+		  return Math.floor((Math.random() * GameSettings.maxNumber)+1);
 
 	}
 }
@@ -32,34 +40,31 @@ var Game = function(){
 Game.prototype.start = function() {
   console.log("game started");
 
-	this.currentNumber = this.generateNumber(100);
-	this.nextNumber = this.generateNumber(100);
+	this.currentNumber = this.generateNumber();
+	this.nextNumber = this.generateNumber();
 
   console.log(currentNumber);
 };
 
 Game.prototype.gameStep = function(option)
 {
-	//var nextNumber = this.generateNumber(100);
-
 	if(this.compare(option, this.nextNumber))
 	{
 		this.score ++;
 		this.currentNumber = this.nextNumber;
-		this.nextNumber = this.generateNumber(100);
+		this.nextNumber = this.generateNumber();
 		return 1;
 	}
 	else
 	{
-		this.lifes --;
-		if(this.lifes > 0)
+		if(this.lives > 0)
 		{
-		return 0;
+				this.lives --;
+				return 0;
 		}
 		else
 		{
 			return -1;
 		}
 	}
-
 }
